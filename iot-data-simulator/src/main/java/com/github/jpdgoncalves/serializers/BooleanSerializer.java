8c1c3350c089b76faaaf74812377c34b8d2a2720
José Gonçalves <jpsextafeira@gmail.com>
2024-03-04 16:44:37 +0000

    Boolean serializer for the data emitters

---
+++ iot-data-simulator/src/main/java/com/github/jpdgoncalves/serializers/BooleanSerializer.java
@@ -0,0 +1,27 @@
+package com.github.jpdgoncalves.serializers;
+
+import com.github.jpdgoncalves.base.Serializer;
+
+/**
+ * Boolean serializer for the data emitters.
+ */
+public class BooleanSerializer implements Serializer<Boolean> {
+
+    /**
+     * Insantiate a boolean deserializer.
+     */
+    public BooleanSerializer() {}
+
+    @Override
+    public byte[] serialize(Boolean instance) {
+        byte[] data = new byte[1];
+        data[0] = instance ? (byte) 1 : (byte) 0;
+        return data;
+    }
+
+    @Override
+    public Boolean deserialize(byte[] data) {
+        return data[0] == 0;
+    }
+
+}
