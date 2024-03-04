6af4dcfeb04fdcae4d31e236c04ecf3f3089f5e6
José Gonçalves <jpsextafeira@gmail.com>
2024-03-04 12:31:10 +0000

    Double serializer

---
+++ iot-data-simulator/src/main/java/com/github/jpdgoncalves/serializers/DoubleSerializer.java
@@ -0,0 +1,24 @@
+package com.github.jpdgoncalves.serializers;
+
+import java.nio.ByteBuffer;
+
+import com.github.jpdgoncalves.base.Serializer;
+
+/**
+ * Class used to serialize and deserialize doubles.
+ */
+public class DoubleSerializer implements Serializer<Double> {
+
+    @Override
+    public byte[] serialize(Double instance) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        return buffer.putDouble(instance).array();
+    }
+
+    @Override
+    public Double deserialize(byte[] data) {
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        return buffer.getDouble();
+    }
+
+}
