aaf167e55f718b0b1fb0b046f33f2c5342c396e5
José Gonçalves <jpsextafeira@gmail.com>
2024-03-06 15:42:28 +0000

    Template for sensors test

---
+++ iot-data-simulator/src/test/java/test/iotdatasim/sensor/SensorsTest.java
@@ -0,0 +1,10 @@
+package test.iotdatasim.sensor;
+
+/**
+ * Test that ensures that all sensor simulators
+ * output the expected values when given a certain
+ * input (seed and a clock to get the current time).
+ */
+public class SensorsTest {
+
+}
