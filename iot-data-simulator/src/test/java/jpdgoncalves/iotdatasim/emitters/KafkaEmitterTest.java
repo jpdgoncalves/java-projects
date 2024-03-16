7cc8b6ed2ac58bce3d288af3dec4fb0bf791652a
José Gonçalves <jpsextafeira@gmail.com>
2024-03-16 13:56:02 +0000

    Test for integration with kafka

--- iot-data-simulator/src/test/java/jpdgoncalves/iotdatasim/emitters/KafkaEmitterTest.java
+++ iot-data-simulator/src/test/java/jpdgoncalves/iotdatasim/emitters/KafkaEmitterTest.java
@@ -1,11 +1,15 @@
 package jpdgoncalves.iotdatasim.emitters;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import jpdgoncalves.iotdatasim.base.SensorSimulator;
+import jpdgoncalves.iotdatasim.base.SensorTicker;
 import jpdgoncalves.iotdatasim.sensor.DefaultHumiditySensor;
 import jpdgoncalves.iotdatasim.serializers.DoubleSerializer;
 import jpdgoncalves.iotdatasim.serializers.KafkaSerializer;
@@ -13,14 +17,31 @@ import jpdgoncalves.iotdatasim.serializers.KafkaSerializer;
 public class KafkaEmitterTest {
 
     @Test
-    public void testEmitter() {
+    @Timeout(20)
+    public void testEmitter() throws InterruptedException {
 
-        final long period = 0;
+        final long period = 1000;
         final String topic = "humidity";
 
         SensorSimulator<Double> sensor = new DefaultHumiditySensor(1289073459497L);
+        SensorTicker ticker = new SensorTicker(period);
+
         Serializer<Double> valueSerializer = new KafkaSerializer<>(new DoubleSerializer());
         MockProducer<String, Double> mockProducer = new MockProducer<>(true, new StringSerializer(), valueSerializer);
         KafkaEmitter<Double> emitter = new KafkaEmitter<>(period, topic, sensor, mockProducer);
+
+        ticker.addSensor(sensor);
+        ticker.start();
+        emitter.start();
+
+        Thread.sleep(period * 10);
+
+        emitter.interrupt();
+        ticker.interrupt();
+
+        emitter.join(2000);
+        ticker.join(2000);
+
+        assertTrue(mockProducer.history().size() > 9);
     }
 }
